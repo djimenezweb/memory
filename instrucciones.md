8572c46236f07833179f2b4dd61042102ec32645
Daniel Jiménez <djimenezweb@gmail.com>
2023-01-27 18:56:03 +0100

    first commit

---
+++ instrucciones.md
@@ -0,0 +1,27 @@
+## Pasos a seguir
+
+- En el HTML crea la estructura de la carta de referencia
+
+```html
+<div class="card">
+  <div class="card__front">
+    <img src="assets/images/46.png" class="card__image" />
+  </div>
+  <div class="card__back"></div>
+</div>
+```
+
+- Con CSS haz que la parte trasera se vea como una tarjeta pokemon.
+
+- Consigue que al cargar la página la tarjeta gire, te muestre el pokemon y se vuelva a girar.
+
+- Crea en el HTML 3 cartas más para tener dos parejas de pokemons.
+
+- Replica el comportamiento de que las tarjetas giren y se vuelvan a ocultar al cargar la página.
+
+- Haz que las tarjetas giren al hacer click sobre ellas, si la pareja es correcta no se darán la vuelta, si no es correcta se girarán de nuevo. RECUERDA que existen los atributos data- SON MUY ÚTILES 😊
+
+## SI ALGO FALLA (que lo hará)
+
+- Asegurate de que estás haciendo click en el elemento que quieres.
+- Asegúrate de que los datos que comparas son los que realmente quieres comparar y son del tipo de dato que esperas.
--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,46 @@
-// El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
-import { sayHello } from './demo.js';
+const allCards = document.querySelectorAll('.card');
 
-sayHello();
+let firstChoice;
+let secondChoice;
+
+const flipCard = card => {
+  console.log('CARD---', card);
+  card.classList.toggle('card--show');
+};
+
+const checkCards = () => {
+  console.log(firstChoice.dataset.pokemon === secondChoice.dataset.pokemon);
+  if (firstChoice.dataset.pokemon !== secondChoice.dataset.pokemon) {
+    flipCard(firstChoice);
+    flipCard(secondChoice);
+  }
+  firstChoice = undefined;
+  secondChoice = undefined;
+};
+
+const asignChoice = card => {
+  console.log(card.dataset.selected);
+  if (card.dataset.selected) return;
+  card.dataset.selected = true;
+  firstChoice ? (secondChoice = card) : (firstChoice = card);
+  flipCard(firstChoice);
+  if (firstChoice && secondChoice) checkCards();
+};
+
+document.addEventListener('click', ev => {
+  if (!ev.target.classList.contains('card')) return;
+  asignChoice(ev.target);
+});
+
+window.addEventListener('load', () => {
+  allCards.forEach(card => {
+    flipCard(card);
+  });
+  const timeoutId = setTimeout(() => {
+    allCards.forEach(card => {
+      flipCard(card);
+    });
+    clearTimeout(timeoutId);
+  }, 1000);
+});
